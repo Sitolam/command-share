99135d3e84a68cd247fe61066f7d797c01f876db
sitolam <79202140+Sitolam@users.noreply.github.com>
2023-09-16 15:02:07 +0200

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cat << EOF >> /etc/profile
+#locale settings
+export LANG=en_US.UTF-8
+#export LANGUAGE="en_US:en"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_CTYPE="nl_BE@euro"
+export LC_COLLATE="nl_BE@euro"
+export LC_TIME="nl_BE"
+export LC_NUMERIC="nl_BE"
+export LC_MONETARY="nl_BE@euro"
+export LC_PAPER="nl_BE"
+export LC_TELEPHONE="nl_BE"
+export LC_ADDRESS="nl_BE"
+export LC_MEASUREMENT="nl_BE"
+export LC_NAME="nl_BE"
+EOF
+cat > /etc/locale.conf << EOF
+#locale settings
+LANG=en_US.UTF-8
+#export LANGUAGE="en_US:en"
+LC_MESSAGES="en_US.UTF-8"
+LC_CTYPE="nl_BE@euro"
+LC_COLLATE="nl_BE@euro"
+LC_TIME="nl_BE"
+LC_NUMERIC="nl_BE"
+LC_MONETARY="nl_BE@euro"
+LC_PAPER="nl_BE"
+LC_TELEPHONE="nl_BE"
+LC_ADDRESS="nl_BE"
+LC_MEASUREMENT="nl_BE"
+LC_NAME="nl_BE"
+EOF
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
