5c3395d1bb422a6272990554ddd525695d2d3490
sitolam <79202140+Sitolam@users.noreply.github.com>
2023-10-08 19:06:35 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i 's/HOOKS=(base udev autodetect modconf kms keyboard keymap consolefont block filesystems fsck)/HOOKS=(base udev block modconf kms keyboard keymap consolefont autodetect filesystems fsck)/g' /etc/mkinitcpio.conf
+echo "test"
